fc0a1d953e7f3ba46126de1458bdc2934b6c0ab8
Julius Daub <julius@daubweb.com>
2020-12-15 18:32:34 +0100

    Cleaned App.py

--- src/app.py
+++ src/app.py
@@ -4,9 +4,13 @@ app.py
 
 The core module of this project, serves as *frontend*
 """
-
+import spacy
 import streamlit as st
-from utils.dialogue_parser import dialogue_parser
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+nlp = spacy.load("de_core_news_md")
+
+from utils.dialogue_parser import DialogueParser, preprocess
 from docx import Document
 
 from utils.feature_annotator import add_features, reading_ease_german
@@ -15,27 +19,55 @@ st.markdown("# Welcome to Sigmund")
 
 uploaded_docx = st.file_uploader("Upload docx File here!")
 
+
+def f(words, sentences):
+    return words / sentences
+
+
 if uploaded_docx:
     st.write("A file was uploaded!")
-    parsed_dialogue = dialogue_parser(uploaded_docx)
-    add_features(parsed_dialogue)
-    """
-    Something important to note!
-    """
-    shares = parsed_dialogue.groupby(["Speaker"]).sum()["word_count"]
-    
-    concat_per_speaker = parsed_dialogue.groupby(['Speaker'], as_index = False).agg({'Text': ' '.join})
-    concat_per_speaker["FleshScore"] = concat_per_speaker["Text"].apply(reading_ease_german)
-    st.write(concat_per_speaker)
-    # for paragraph in paragraphs:
-    #     st.write(paragraph.text)
+    group = st.text_input("Gruppe")
+    is_depressed = st.checkbox("Depressiv?")
+    female_label = st.selectbox("Sprecher W", ["A", "B"])
+    couple_id = st.text_input("ID")
+    dp = DialogueParser(uploaded_docx, group, couple_id=couple_id, female_label=female_label, depressed=is_depressed)
+    preprocessed_df = dp.get_paragraphs().apply(preprocess, axis=1)
+    st.write(dp.get_paragraphs())
+    st.write(preprocessed_df)
+    # add_features(parsed_dialogue)
 
-    st.markdown("## Metrics")
 
-    a_share = st.write("Speaker A Share")
-    st.progress(shares["A: "] / shares.sum())
-    st.write("Speaker B Share")
-    st.progress(shares["B: "] / shares.sum())
 
-    st.markdown("## Dialogue as Data-Frame")
-    st.write(parsed_dialogue)
+    # concat_per_speaker = parsed_dialogue.groupby(['Speaker'], as_index=False).agg(
+    #     {'Text': ' '.join, "Sentences": "sum", "word_count": "sum"})
+    # concat_per_speaker["FleshScore"] = concat_per_speaker["Text"].apply(reading_ease_german)
+    # concat_per_speaker['Avg Sentence Length'] = concat_per_speaker[['word_count', 'Sentences']].apply(lambda x: f(*x),
+    #                                                                  axis=1)
+    # st.write(concat_per_speaker)
+
+    # corpus = concat_per_speaker["Text"]
+    # vectorizer = TfidfVectorizer()
+    # vectorizer.fit(corpus)
+    #
+    # x = vectorizer.transform(corpus)
+    # st.write(vectorizer.transform(concat_per_speaker[concat_per_speaker["Speaker"] == "A: "]["Text"]).toarray())
+    # st.write(vectorizer.transform(concat_per_speaker[concat_per_speaker["Speaker"] == "B: "]["Text"]).toarray())
+    # st.write(x.toarray())
+
+    # speaker_b_tfidf = vectorizer.transform(concat_per_speaker[concat_per_speaker["Speaker"] == "B: "])
+
+
+    # st.write(speaker_b_tfidf.toarray().shape)
+
+    # st.markdown("## Metrics")
+    #
+    # a_share = st.write("Speaker A Share")
+    # st.progress(shares["A: "] / shares.sum())
+    # st.write("Speaker B Share")
+    # st.progress(shares["B: "] / shares.sum())
+    #
+    # st.markdown("## Dialogue as Data-Frame")
+    # st.write(parsed_dialogue)
+    # st.write(len(parsed_dialogue))
+    # st.write(sent_df)
+    # st.write(len(sent_df))

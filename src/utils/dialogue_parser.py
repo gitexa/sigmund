6d9a83203cda73d25ec90bf76f7efd0d4bd77e93
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-01-18 16:52:32 +0100

    Dialogue Parser written by Julius :thumbup:

--- src/utils/dialogue_parser.py
+++ src/utils/dialogue_parser.py
@@ -1,22 +1,22 @@
+import copy
 import re
 import string
-from copy import copy
+from collections import defaultdict
 
 import docx
-import numpy as np
-import pandas
 import pandas as pd
 import spacy
-from prettytable import PrettyTable, prettytable
-from sklearn.pipeline import Pipeline
-
-nlp = spacy.load("de_core_news_md")
-nlp.add_pipe(nlp.create_pipe('sentencizer'))
 
 
 class DialogueParser:
     @staticmethod
     def split_speaker_text(text_line):
+        """
+        Splits Speaker and Text of paragraph
+        :type text_line: str
+        :param text_line: Paragraph to split
+        :return: tuple of (speaker, text), if no speaker: (None, Text)
+        """
         pattern = re.compile(r"^(([AB])[:;] )(.*)")
         matches = pattern.match(text_line)
 
@@ -25,112 +25,133 @@ class DialogueParser:
         else:
             return None, None
 
-    def __init__(
-            self, doc_file, group, couple_id, female_label="B", depressed=None,
-            remove_annotations=True):
+    @staticmethod
+    def clean_comments(text: str) -> str:
+        """
+        Remove Comments from Text
+        :type text: str
+        :param text: text to remove comments from
+        :return: cleaned text
+        """
+        if text:
+            text = re.sub(r"\(.*?\)", "", text)
+            text = re.sub(r"\[.*?\]", "", text)
+            # Remove Digits and Symbols
+            # Remove whitespace
+            text = re.sub(' +', ' ', text)
+            text = re.sub(r' \.', '.', text)
 
-        self.female_label = female_label
-        self.group = group
-        self.couple_id = couple_id
-        self.docx = docx.Document(doc_file)
-        self.is_depressed = depressed
-        self.id = id
+            return text
+        else:
+            return " "
 
+    def split_sentences(self, text, using_punctuation=True):
+        if not using_punctuation:
+            text = ''.join([symb for symb in text if not (
+                symb.isdigit() or symb in string.punctuation)])
+        doc = self.nlp(text)
+
+        sentences = [sent.text for sent in doc.sents]
+
+        return sentences
+
+    def __init__(self, file, nlp, clean_comments=True, genders=None) -> None:
+        """
+        Create Parser Object
+        :type file: str
+        :type nlp: callable
+        :type clean_comments: Bool
+        :param file: Doc-Object to parse
+        :param nlp: NLP Object to use to split sentences
+        :param clean_comments: clean comments like "Unverständlich", default: True
+        """
+        self.document = docx.Document(file)
+
+        self.nlp = nlp
+        if not self.nlp.has_pipe("sentencizer"):
+            self.nlp.add_pipe(self.nlp.create_pipe('sentencizer'))
+
+        dictionary_rows = []
+        sentence_dict_row = []
+        self.paragraphs = [paragraph.text for paragraph in self.document.paragraphs]
+        self.genders = {}
+        if genders:
+            self.genders["A"], self.genders["B"] = genders
+        else:
+            self.genders["A"] = self._extract_gender(self.paragraphs[1])
+            self.genders["B"] = self._extract_gender(self.paragraphs[2])
+
+        print(self.genders)
+
+        for paragraph_position, paragraph in enumerate(
+                list(self.document.paragraphs)[3:]):
+            dictionary_row = {}
+            dictionary_row["paragraph_position"] = paragraph_position
+            speaker, text = self.split_speaker_text(paragraph.text)
+
+            if clean_comments:
+                text = DialogueParser.clean_comments(text)
+
+            if speaker:
+                dictionary_row["speaker"] = speaker
+                dictionary_row["gender"] = self.genders[speaker]
+                dictionary_row["raw_text"] = text
+                dictionary_row["is_paragraph"] = True
+                sentences = self.split_sentences(text, True)
+                for index, sentence in enumerate(sentences):
+                    sentence_row = copy.copy(dictionary_row)
+                    sentence_row["raw_text"] = sentence
+                    sentence_row["sentence_index"] = index
+                    sentence_dict_row.append(sentence_row)
+            else:
+                dictionary_row["raw_text"] = paragraph.text
+                dictionary_row["is_paragraph"] = False
+            dictionary_rows.append(dictionary_row)
+
+        self.paragraph_frame = pd.DataFrame(dictionary_rows)
+        self.sentence_frame = pd.DataFrame(sentence_dict_row)
         super().__init__()
 
-    # @ToDo Deal with single Characters and whitespaces interpreted as sentences
-    def get_sentences(self, remove_annotations=True, drop_na=True):
-        rows = []
-        for i, p in enumerate(self.docx.paragraphs[3:]):
-            speaker, text = self.split_speaker_text(p.text)
+        self.df_paragraphs = pd.DataFrame()
 
-            gender = "W" if speaker == self.female_label else "M"
-            depression_label = (gender == "W" and self.is_depressed)
+    def _extract_gender(self, paragraph):
+        without_prefix = re.sub("[AB]: ", "", paragraph)
+        without_prefix = re.sub("Er", "M", without_prefix)
+        without_prefix = re.sub("Sie", "W", without_prefix)
+        return f"{without_prefix}"
 
-            if speaker:
-                if remove_annotations:
-                    text = re.sub(r"\(.*\)\w?", "", text)
-                    text = re.sub(r"\[.*\]\w?", "", text)
-                doc = nlp(text)
-                for j, sent in enumerate(doc.sents):
-                    row = {
-                        "par_pos": i,
-                        "sent_pos": j,
-                        "speaker": speaker,
-                        "raw_text": sent.text,
-                        "group": self.group,
-                        "gender": gender,
-                        "is_depressed": depression_label,
-                        "couple_id": self.couple_id}
-                    rows.append(row)
+    def get_sentences(self, query=None):
+        """
+                Get sentences of Dialogue as Dataframe, Meta-Data and raw text
+                :type query: additional filter for sentences to get, example: get all paragraphs of speaker A
+                """
+        if query:
+            return self.sentence_frame.query(query)
+        return self.sentence_frame
 
-        sentence_df = pd.DataFrame(rows)
+    def get_paragraphs(self, query: string = None):
+        """
+        Get paragraphs of Dialogue as Dataframe, Meta-Data and raw text
+        :param query: additional filter for paragraphs to get, example: get all paragraphs of speaker A
+        """
+        if query:
+            return self.paragraph_frame.query(query)
+        return self.paragraph_frame
 
-        if drop_na:
-            sentence_df.dropna(inplace=True)
-
-        return sentence_df
-
-    def get_paragraphs(self, remove_annotations=True, drop_na=True):
-        rows = []
-        for i, p in enumerate(self.docx.paragraphs[3:]):
-            speaker, text = self.split_speaker_text(p.text)
-            gender = "W" if speaker == self.female_label else "M"
-            depression_label = (gender == "W" and self.is_depressed)
-            if speaker:
-                if remove_annotations:
-                    text = re.sub(r"\(.*\)\w?", "", text)
-                    text = re.sub(r"\[.*\]\w?", "", text)
-                row = {
-                    "par_pos": i,
-                    "speaker": speaker,
-                    "raw_text": text,
-                    "group": self.group,
-                    "gender": gender,
-                    "is_depressed": depression_label,
-                    "couple_id": self.couple_id,
-                }
-                rows.append(row)
-
-        paragraph_df = pd.DataFrame(rows)
-
-        if drop_na:
-            paragraph_df.dropna(inplace=True)
-
-        return paragraph_df
-
-    def get_fulltext(self):
-        return self.get_paragraphs().groupby(
-            ['gender', "is_depressed"],
-            as_index=False).agg(
-            {'raw_text': ' '.join})
-
-
-def clear_annotations(raw_text):
-    removed_annotations = re.sub(r"\(.*\)", "", raw_text)
-    removed_annotations = ' '.join(removed_annotations.split())
-    return removed_annotations
-
-
-def preprocess(row):
-    raw_text = clear_annotations(str.lower(row["raw_text"]))
-    doc = nlp(raw_text)
-    row["sent_count"] = len(list(doc.sents))
-    doc = nlp(' '.join([token.text for token in doc if not token.is_punct]))
-    row["word_count"] = len(list(doc))
-    row["lemmatized"] = ' '.join(
-        [token.lemma_ if not token.pos_ == "PRON" else token.text for token in doc])
-    doc = nlp(row["lemmatized"])
-    row["stopwords_removed"] = ' '.join(
-        [token.text for token in doc if not token.is_stop])
-    # Normalized without
-    row["normalized_text"] = doc.text
-    return row
+    def get_fulltext(self, split_speakers=True):
+        if split_speakers:
+            return self.paragraph_frame.groupby(by="speaker").agg({"raw_text": ''.join})
+        else:
+            return self.paragraph_frame["raw_text"].str.cat(sep=" ")
 
 
 if __name__ == '__main__':
-    dp_1 = DialogueParser(
-        r"C:\Users\juliusdaub\PycharmProjects\sigmund\data\Paar 27_T1_IM_FW.docx",
-        "DEPR", 27, "B", True)
-    sentences = dp_1.get_sentences(drop_na=False)
-    print(sentences.to_markdown())
+    dp = DialogueParser(r"../data/Paar 105_T1_IM_FW.docx",
+                        spacy.load("de_core_news_md"),
+                        clean_comments=True)
+    sentences = dp.get_sentences()
+    fulltext = dp.get_fulltext(False)
+    sentences_a = dp.get_sentences(query="speaker=='A'")
+    sentences_b = dp.get_sentences(query="speaker=='B'")
+    print(sentences_b)
+    pass

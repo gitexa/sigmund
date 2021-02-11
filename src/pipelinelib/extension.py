395693ec44c90d2c27428382c033fc0c013c3b42
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-02-11 16:06:55 +0100

    Simplified loading and storing to / from Doc

--- src/pipelinelib/extension.py
+++ src/pipelinelib/extension.py
@@ -1,3 +1,8 @@
+import operator
+
+from spacy.tokens import Doc
+
+
 class Extension:
     """
     A class to represent an extension member of spacy's
@@ -13,6 +18,14 @@ class Extension:
         initialised with.
         This should not change during runtime
     """
+
     def __init__(self, name: str, default_type):
         self.name = name
         self.default_type = default_type
+
+    def load_from(self, doc: Doc):
+        return operator.attrgetter(self.name)(doc._)
+
+    def store_to(self, doc: Doc, value):
+        # TODO: operator.attrsetter version
+        doc._[self.name] = value

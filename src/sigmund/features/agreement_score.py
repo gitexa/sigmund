701cc6596445fe271c29c8337714f05977e8f514
Ubeyd <ubeydullah.il@googlemail.com>
2021-02-17 16:05:06 +0100

    Working AgreementScore-Port

--- src/sigmund/features/agreement_score.py
+++ src/sigmund/features/agreement_score.py
@@ -2,23 +2,27 @@ import re
 import string
 from collections import Counter
 from itertools import filterfalse
+from typing import Dict, List
 
 import liwc
+import numpy as np
+import pandas as pd
 import pyphen
 import spacy
 from spacy.tokens import Doc
 
 from src.pipelinelib.component import Component
 from src.pipelinelib.extension import Extension
-
-nlp = spacy.load("de_core_news_md")
+from src.pipelinelib.querying import Queryable
+from src.pipelinelib.text_body import TextBody
+from src.sigmund.extensions import AGREEMENTSCORE, TALKTURN, TOKENS
 
 
 class AgreementScoreExtractor(Component):
     """
     Extracts the Agreement-Score turn from text and stores these under doc._.agreementscore
     """
-    agreementscore = Extension(name="agreementscore", default_type=list())
+    agreementscore = Extension(name="agreementscore")
 
     def __init__(self, dictionary_path: str):
         super().__init__(name=AgreementScoreExtractor.__name__, required_extensions=list(),
@@ -26,70 +30,68 @@ class AgreementScoreExtractor(Component):
         self.dic = pyphen.Pyphen(lang='de')
         self._dictionary_path = dictionary_path
 
-    def apply(self, doc: Doc) -> Doc:
+    def apply(self, storage: Dict[Extension, pd.DataFrame],
+              queryable: Queryable) -> Dict[Extension, pd.DataFrame]:
 
         # Load LIWC Dictionary provided by path
         parse, category_names = liwc.load_token_parser(self._dictionary_path)
         # neg : negate(7) ,negemo(16),  23	Discrep,  45	Excl
         disagreement_cat = ['Negate', 'Negemo', 'Discrep', 'Excl']
 
-        tmp = doc.text.split("$")
-        tmp = [tmp[0].split("|"), tmp[1].split("|")]
+        # Can be replaced when access to TOKENS is clear
+        tokens = queryable.execute(level=TextBody.PARAGRAPH)
+        tokens = tokens[['document_id', 'paragraph_id', 'speaker', 'text']]
+        tokens['text'] = tokens['text'].apply(tokenize_df, nlp=queryable.nlp)
+
+        # Get number of documents
+        doc_count = tokens['document_id'].max()
+        paragraph_count_per_doc = tokens.groupby(['document_id'])['paragraph_id'].max()
+
+        tokens = tokens.groupby(['document_id'])['text'].apply(list).to_dict()
 
         agr_score = [0, 0]
-        agr_score[1] = len(tmp[0]) + len(tmp[1])
-        for x in range(len(tmp)):
-            for y in range(len(tmp[x])):
-                # nlp(tmp[x][y])
-                #map(str, nlp(tmp[x][y]))
-                #print(list(filterfalse(string.punctuation.__contains__, map(str, nlp(tmp[x][y])))))
-                #print(len(list(filterfalse(string.punctuation.__contains__, map(str, nlp(tmp[x][y]))))))
+        agr_score[0] = np.zeros(doc_count + 1)
+        agr_score[1] = paragraph_count_per_doc.to_numpy()
 
-                if len(
-                    list(
-                        filterfalse(
-                            string.punctuation.__contains__,
-                            map(str, nlp(tmp[x][y]))))) > 5:
-                    tokens = [
-                        x.lower()
-                        for x in list(
-                            filterfalse(
-                                string.punctuation.__contains__,
-                                map(str, nlp(tmp[x][y]))))[: 5]]
+        for x in range(doc_count+1):
+            for y in range(len(tokens[x])):
+                if len(tokens[x][y]) > 5:
 
-                    flag = False
                     for category in disagreement_cat:
-                        if Counter(category for token in tokens
+                        if Counter(category for token in tokens[x][y][:5]
                                    for category in parse(token))[category] >= 1:
-
-                            print(x, y, category, tokens)
-                            if flag == False:
-                                agr_score[0] += 1
-                            flag = True
+                            # print(x, y, category, tokens[x][y][:5])
+                            agr_score[0][x] += 1
+                            break
                 else:
-                    tokens = [
-                        x.lower()
-                        for x in list(
-                            filterfalse(
-                                string.punctuation.__contains__,
-                                map(str, nlp(tmp[x][y]))))]
-
-                    flag = False
                     for category in disagreement_cat:
-                        if Counter(category for token in tokens
+                        if Counter(category for token in tokens[x][y]
                                    for category in parse(token))[category] >= 1:
+                            # print(x, y, category, tokens[x][y][:5])
+                            agr_score[0][x] += 1
+                            break
 
-                            print(x, y, category, tokens)
-                            if flag == False:
-                                agr_score[0] += 1
-                            flag = True
+        agr_score = 1 - np.around(agr_score[0] / agr_score[1], decimals=2)
 
-        doc._.agreementscore = round(agr_score[0] / agr_score[1], 2)
-        print(agr_score)
-        return doc
+        # Prepare data for Dataframe
+        values = np.concatenate(
+            (np.arange(0, doc_count + 1).astype(int),
+             agr_score),
+            axis=0).reshape((2, 10)).transpose()
+
+        agr_score = pd.DataFrame(values,
+                                 columns=['document_id', 'AgreementScore'])
+
+        return {AGREEMENTSCORE: agr_score}
 
 
-def tokenize(text):
-    # you may want to use a smarter tokenizer
-    for match in re.finditer(r'\w+', text, re.UNICODE):
-        yield match.group(0)
+def tokenize_df(sentence: str, nlp) -> List[str]:
+    tokens = nlp(sentence)
+    res = []
+    # Go through tokens and check if it is inside the punctuation set
+    # If this is the case it will be ignored
+    for token in map(str, tokens):
+        if not any(p in token for p in string.punctuation):
+            res.append(token.lower())
+
+    return res

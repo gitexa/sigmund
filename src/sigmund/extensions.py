09ece5c9352c8f8cebec5e502b74e876d9e57a9b
Alexander Haas <haas.alexanderjulian@gmail.com>
2021-02-17 15:38:30 +0100

    Added extenstions

--- src/sigmund/extensions.py
+++ src/sigmund/extensions.py
@@ -4,6 +4,9 @@ from src.pipelinelib.extension import Extension
 TOKENS_SENTENCE = Extension("tokens_sentence")
 TOKENS_PARAGRAPHS = Extension("tokens_paragraphs")
 TOKENS_DOCUMENT = Extension("tokens_document")
+
+WO_ANNOTATIONS = Extension("wo_annotations")
+WO_STOPWORDS = Extension("wo_stopwords")
 STEMMED = Extension("stemmed")
 LEMMATIZED = Extension("lemmatized")
 SYLLABLES = Extension("syllabels")

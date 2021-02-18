08bcd76f4ca1e93a5670c805b0ed380157683a3e
Alexander Haas <haas.alexanderjulian@gmail.com>
2021-02-18 21:47:25 +0100

    Finished porting of tfidf

--- src/sigmund/features/tfidf.py
+++ src/sigmund/features/tfidf.py
@@ -1,24 +1,17 @@
 import operator
 import re
 import string
-from collections import Counter
-from itertools import filterfalse
 from typing import Dict
 
-import liwc
 import pandas as pd
 from IPython.core.display import display
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from spacy.tokens import Doc
 
 from src.pipelinelib.component import Component
 from src.pipelinelib.extension import Extension
 from src.pipelinelib.querying import Queryable
 from src.pipelinelib.text_body import TextBody
-from src.sigmund.extensions import (LEMMATIZED_DOCUMENT, LEMMATIZED_SENTENCE,
-                                    STEMMED_DOCUMENT, STEMMED_SENTENCE, TFIDF_DOCUMENT,
-                                    TOKENS_SENTENCE)
-
+from src.sigmund.extensions import *
 
 class FeatureTFIDF(Component):
     """
@@ -36,16 +29,19 @@ class FeatureTFIDF(Component):
 
         # Get transcipts on document level stemmed
         df_lemmatized_document = LEMMATIZED_DOCUMENT.load_from(storage)
-        display(df_lemmatized_document)
         df_tfidf_document = self.get_tfidf(df_lemmatized_document)
 
         return {TFIDF_DOCUMENT: df_tfidf_document}
 
     def get_tfidf(self, lines):
 
+        # join text to string and retokenize with min_frequency
+        lines['text'] = lines['text'].apply(
+            lambda row: ' '.join(token for token in row))
+
         # get vectors with frequencies for the words in the lines; each line is considered a document; remove stop words with stop-word-list from scikit-learn; exclude words with frequency smaller 5
-        count_vectorizer = CountVectorizer(min_df=1)
-        count_vectorized = count_vectorizer.fit_transform(lines)
+        count_vectorizer = CountVectorizer(min_df=5)
+        count_vectorized = count_vectorizer.fit_transform(lines['text'])
 
         # transform the vector-frequency matrix in tfidf
         tfidf_transformer = TfidfTransformer(use_idf=True)
@@ -54,10 +50,12 @@ class FeatureTFIDF(Component):
         # create new dataframe with tfidfs and the feature names; calculate the mean tfidf over all lines/documents
         df_tfidf = pd.DataFrame(
             tfidf.T.todense(),
-            index=count_vectorizer.get_feature_names())
-        df_tfidf['mean'] = df_tfidf.mean(axis=1)
-        df_tfidf = df_tfidf.sort_values('mean', ascending=False)
+            index=count_vectorizer.get_feature_names()).T
 
-        display(df_tfidf)
+        # construct results with couple_id
+        df_tfidf.insert(loc=0, column='couple_id', value=lines['couple_id'])
 
-        return df_tfidf['mean']
+        # select only columns which differ between depressed and non-depressed couples
+        # TODO if desired
+
+        return df_tfidf

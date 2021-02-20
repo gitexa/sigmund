b78efffc3c2ed5f117618224de597e825910f418
Alexander Haas <haas.alexanderjulian@gmail.com>
2021-02-20 15:34:25 +0100

    Initial porting of NB classifier

--- src/sigmund/classification/naive_bayes.py
+++ src/sigmund/classification/naive_bayes.py
@@ -8,6 +8,26 @@ from pipelinelib.extension import Extension
 from sigmund.features.tfidf import TfIdf
 
 
+class NaiveBayes(Component):
+    """
+    Performs naive bayes on a feature dataframe 
+    """
+
+    def __init__(self):
+        super().__init__(
+            NaiveBayes.__name__,
+            required_extensions=[FEATURE_VECTOR],
+            creates_extensions=[CLASSIFICATION_NAIVE_BAYES])
+
+    def apply(self, storage: Dict[Extension, pd.DataFrame],
+              queryable: Queryable) -> Dict[Extension, pd.DataFrame]:
+
+        # Document
+        df_feature_vector = FEATURE_VECTOR.load_from(storage=storage)
+
+        return {CLASSIFICATION_NAIVE_BAYES: df_stemmed_sentence}
+
+
 class NaiveBayesOnTfIdf(Component):
     BAYES = Extension("tfidf_gaussian_nb", int)
 

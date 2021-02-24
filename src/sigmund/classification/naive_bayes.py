e2044eb986337db8d61d2cfc557626804aa8a1f0
Alexander Haas <haas.alexanderjulian@gmail.com>
2021-02-25 00:42:39 +0100

    Update reading

--- src/sigmund/classification/naive_bayes.py
+++ src/sigmund/classification/naive_bayes.py
@@ -122,7 +122,7 @@ class NaiveBayes(Component):
 
                 # construct train and test set
                 features_train, features_test, label_train, label_test, indices_train, indices_test = train_test_split(
-                    features, labels, features.index.values, test_size=0.50, random_state=42)
+                    features, labels, features.index.values, test_size=0.50)
 
                 # if training_mode
                 if not self.evaluate_model:
@@ -318,7 +318,7 @@ class NaiveBayes(Component):
 
                 # construct the train and test set for hamilton
                 features_train, features_test, label_train, label_test, indices_train, indices_test = train_test_split(
-                    features, labels, features.index.values, test_size=0.50, random_state=42)
+                    features, labels, features.index.values, test_size=0.50)
 
                 # if training_mode
                 if not self.evaluate_model:

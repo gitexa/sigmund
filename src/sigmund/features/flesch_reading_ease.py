129749ef0033e64e0bc8f22ec2bf69e4d3c2ae71
Ubeyd <ubeydullah.il@googlemail.com>
2021-02-25 14:15:04 +0100

    Bug is fixed

--- src/sigmund/features/flesch_reading_ease.py
+++ src/sigmund/features/flesch_reading_ease.py
@@ -23,9 +23,11 @@ class FleschExtractor(Component):
     """
 
     def __init__(self):
-        super().__init__(name=FleschExtractor.__name__, required_extensions=[],
-                         creates_extensions=[FRE_DOCUMENT_M, FRE_DOCUMENT_F, FRE_DOCUMENT_MF, FRE_PARAGRAPH_M,
-                                             FRE_PARAGRAPH_F, FRE_SENTENCE_M, FRE_SENTENCE_F])
+        super().__init__(
+            name=FleschExtractor.__name__, required_extensions=[],
+            creates_extensions=[FRE_DOCUMENT_M, FRE_DOCUMENT_F, FRE_DOCUMENT_MF,
+                                FRE_PARAGRAPH_M, FRE_PARAGRAPH_F, FRE_SENTENCE_M,
+                                FRE_SENTENCE_F])
         self.dic = pyphen.Pyphen(lang='de')
 
     def apply(self, storage: Dict[Extension, pd.DataFrame],
@@ -36,19 +38,28 @@ class FleschExtractor(Component):
         # fre_document = queryable.execute(level=TextBody.DOCUMENT)
 
         # Filter columns
-        fre_sentence = fre_sentence[['document_id', 'couple_id', 'paragraph_id', 'sentence_id', 'gender', 'text']]
-        fre_paragraph = fre_paragraph[['document_id', 'couple_id', 'paragraph_id', 'gender', 'text']]
-        fre_document = fre_paragraph[['document_id', 'couple_id', 'paragraph_id', 'gender', 'text']]
+        fre_sentence = fre_sentence[['document_id', 'couple_id',
+                                     'paragraph_id', 'sentence_id', 'gender', 'text']]
+        fre_paragraph = fre_paragraph[['document_id',
+                                       'couple_id', 'paragraph_id', 'gender', 'text']]
+        fre_document = fre_paragraph[['document_id',
+                                      'couple_id', 'paragraph_id', 'gender', 'text']]
 
         doc_count = fre_document['document_id'].max()
         couple_ids = fre_document['couple_id'].unique()
 
         # Calculate the Flesch-Reading-Ease
-        fre_sentence['text'] = fre_sentence['text'].apply(self._reading_ease_german, nlp=queryable.nlp())
-        fre_paragraph['text'] = fre_paragraph['text'].apply(self._reading_ease_german, nlp=queryable.nlp())
+        fre_sentence['text'] = fre_sentence['text'].apply(
+            self._reading_ease_german, nlp=queryable.nlp())
+        fre_paragraph['text'] = fre_paragraph['text'].apply(
+            self._reading_ease_german, nlp=queryable.nlp())
 
-        fre_document_M_F = fre_document.groupby(['document_id', 'gender'])['text'].apply(self._process_fre)
-        fre_document_MF = fre_document.groupby(['document_id'])['text'].apply(self._process_fre)
+        fre_document_M_F = fre_document.groupby(
+            ['document_id', 'gender'])['text'].apply(
+            self._process_fre, nlp=queryable.nlp())
+        fre_document_MF = fre_document.groupby(
+            ['document_id'])['text'].apply(
+            self._process_fre, nlp=queryable.nlp())
 
         fre_document_M_F = fre_document_M_F.to_numpy()
         fre_document_MF = fre_document_MF.to_numpy()
@@ -63,8 +74,10 @@ class FleschExtractor(Component):
             axis=0).reshape(
             (5, 10)).transpose()
 
-        fre_document = pd.DataFrame(values, columns=['document_id', 'couple_id', 'fre_document_m', 'fre_document_f',
-                                                     'fre_document_mf'])
+        fre_document = pd.DataFrame(values,
+                                    columns=['document_id', 'couple_id',
+                                             'fre_document_m', 'fre_document_f',
+                                             'fre_document_mf'])
         fre_document['document_id'] = fre_document['document_id'].astype(np.int64)
         fre_document['couple_id'] = fre_document['couple_id'].astype(np.int64)
 
@@ -82,21 +95,30 @@ class FleschExtractor(Component):
         fre_paragraph_m = fre_paragraph_m.rename(columns={'text': 'fre_paragraph_m'})
         fre_paragraph_f = fre_paragraph_f.rename(columns={'text': 'fre_paragraph_f'})
 
-        fre_document_m = fre_document.drop(columns=['fre_document_f', 'fre_document_mf'])
-        fre_document_f = fre_document.drop(columns=['fre_document_m', 'fre_document_mf'])
-        fre_document_mf = fre_document.drop(columns=['fre_document_m', 'fre_document_f'])
+        fre_document_m = fre_document.drop(
+            columns=['fre_document_f', 'fre_document_mf'])
+        fre_document_f = fre_document.drop(
+            columns=['fre_document_m', 'fre_document_mf'])
+        fre_document_mf = fre_document.drop(
+            columns=['fre_document_m', 'fre_document_f'])
 
         # Aggregate over sentences and paragraphs to get document features and drop gender
 
-        fre_sentence_m = fre_sentence_m.drop(columns=['gender', 'paragraph_id', 'sentence_id'])
-        fre_sentence_f = fre_sentence_f.drop(columns=['gender', 'paragraph_id', 'sentence_id'])
+        fre_sentence_m = fre_sentence_m.drop(
+            columns=['gender', 'paragraph_id', 'sentence_id'])
+        fre_sentence_f = fre_sentence_f.drop(
+            columns=['gender', 'paragraph_id', 'sentence_id'])
         fre_paragraph_m = fre_paragraph_m.drop(columns=['gender', 'paragraph_id'])
         fre_paragraph_f = fre_paragraph_f.drop(columns=['gender', 'paragraph_id'])
 
-        fre_sentence_m = fre_sentence_m.groupby(['document_id', 'couple_id']).agg('mean').reset_index()
-        fre_sentence_f = fre_sentence_f.groupby(['document_id', 'couple_id']).agg('mean').reset_index()
-        fre_paragraph_m = fre_paragraph_m.groupby(['document_id', 'couple_id']).agg('mean').reset_index()
-        fre_paragraph_f = fre_paragraph_f.groupby(['document_id', 'couple_id']).agg('mean').reset_index()
+        fre_sentence_m = fre_sentence_m.groupby(
+            ['document_id', 'couple_id']).agg('mean').reset_index()
+        fre_sentence_f = fre_sentence_f.groupby(
+            ['document_id', 'couple_id']).agg('mean').reset_index()
+        fre_paragraph_m = fre_paragraph_m.groupby(
+            ['document_id', 'couple_id']).agg('mean').reset_index()
+        fre_paragraph_f = fre_paragraph_f.groupby(
+            ['document_id', 'couple_id']).agg('mean').reset_index()
 
         return {FRE_DOCUMENT_M: fre_document_m, FRE_DOCUMENT_F: fre_document_f, FRE_DOCUMENT_MF: fre_document_mf,
                 FRE_PARAGRAPH_M: fre_paragraph_m, FRE_PARAGRAPH_F: fre_paragraph_f,
@@ -109,7 +131,7 @@ class FleschExtractor(Component):
         doc = nlp(text)
         syllable_count = 0
         word_count = 0
-        sent_count = len(list(doc.sents))
+        sent_count = len(list(doc.text))
 
         for token in doc:
             if not str(token) in string.punctuation:
